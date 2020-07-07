aa011a9271c1ef5733e0fb09dcc64e404ee3d99c
Camilo Tobon <1611@holbertonschool.com>
2020-07-07 04:40:53 -0500

    agregando /store

--- static/js/app.js
+++ static/js/app.js
@@ -1,9 +1,29 @@
 $(document).ready(() => {
     $.post("/store", null, (data) => {
         let parsed = JSON.parse(data)
-        let section = $("#produ")
+        let section = document.getElementById("productos")
         parsed.forEach(element => {
-            console.log(`Producto: ${element.Precio}`)
+            section.innerHTML += `
+            <div class="card" style="width: 18rem;">
+                <div class="card-body">
+                    <h5 class="card-title">${element.Tipo}</h5>
+                    <p class="card-text">
+                        ${element.Descripcion}\n
+                    </p>
+                    <p class="card-text">
+                        Precio: ${element.Precio}\n
+                    </p>
+                    <p class="card-text">
+                        Talla: ${element.Talla}\n
+                    </p>
+                    <p class="card-text">
+                        Existencias: ${element.Existencias}
+                    </p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+                </div>
+            `
+            console.log(element)
         });
     })
 })
